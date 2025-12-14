8e3bf67b536b275b0c94ff979ebc98c2ee01d8c0
Connor Beaven <Cmbeaven1@gmail.com>
2025-12-14 16:53:11 -0500

    Fix day 05

--- 05.cpp
+++ 05.cpp
@@ -8,8 +8,12 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <algorithm>
 
 typedef std::pair<long,long> range;
+
+bool checkFresh(std::vector<range>& ranges, long ingredient);
+
 
 int main(int argv, char** argc){
     
@@ -32,6 +36,47 @@ int main(int argv, char** argc){
         ingredients.push_back(std::stol(inputLine));
         std::cout << inputLine << '\n';
     }
+    int sum = 0;
+    for(auto ingredient : ingredients){
+        sum += checkFresh(freshRanges,ingredient);
+    }
+    long lsum = 0;
+    // sort vector
+    std::sort(freshRanges.begin(),freshRanges.end(),[](range r1, range r2) { return r1.first < r2.first;});
+
+    for(auto r = freshRanges.begin(); r < freshRanges.end() - 1; ++r){
+        if(r->second >= (r+1)->first){
+            if(r->second > (r+1)->second){
+                (r+1)->second = r->second;
+            }
+            (r+1)->first = r->first;
+            r->first = 0;
+            r->second = 0;
+        }
+    }
+
+    for(auto range : freshRanges){
+        printf("%li-%li\n",range.first,range.second);
+        if(range.first != 0 && range.second != 0){
+            lsum += 1 + range.second - range.first;
+        }
+    }
+
+    printf("fresh: %i\n",sum);
+    printf("fresh total: %li\n",lsum);
+
 
     return 0;
+}
+
+
+bool checkFresh(std::vector<range>& ranges, long ingredient){
+    for(auto range : ranges){
+        if(ingredient >= range.first && ingredient <= range.second){
+            return true;
+        }
+    }
+
+
+    return false;
 }
