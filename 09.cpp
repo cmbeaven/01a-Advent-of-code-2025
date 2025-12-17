dcae3861a94e54d24948c6903c5c1cadfa4413cd
Connor Beaven <Cmbeaven1@gmail.com>
2025-12-17 14:57:46 -0500

    Broken work on day 9 part 2

--- 09.cpp
+++ 09.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+#include <set>
+#include <utility>
+#include <map>
 
 class tileLocation{
     public:
@@ -21,56 +24,52 @@ class tileLocation{
         tileLocation(long x, long y) : x(x), y(y) {}
 };
 
-struct line{
+class line{
+public:
     long x1;
     long x2;
     long y1;
     long y2;
+    line(){x1=0;x2=0;y1=0;y2=0;}
+    line(long x1,long x2,long y1,long y2) : x1(x1), x2(x2), y1(y1), y2(y2) {}
 };
+
+bool determineGreen(tileLocation p,std::vector<line>& hLine, std::vector<line>& vLine){
+    // do a vertical line check first
+    // vertical lines at p.y 
+    std::vector<line> crossedVLine;
+    for(auto l : vLine){
+        if(p.y > std::min(l.y1,l.y2) && p.y < std::max(l.y1,l.y2)){
+            crossedVLine.push_back(l);
+        }
+    }
+    if(crossedVLine.size() == 0) return false;
+    std::sort(crossedVLine.begin(),crossedVLine.end(),[](line l1, line l2){return l1.x1 < l2.x1;});
+    for(auto l : crossedVLine){
+        printf("(%li,%li,%li,%li)",l.x1,l.x2,l.y1,l.y2);
+    }
+    int count = 0;
+    
+
+    // then a horizontal check
+    // horizontal lines at p.x
+    std::vector<line> crossedHLine;
+    for(auto l : hLine){
+        if(p.x > std::min(l.x1,l.x2) && p.x < std::max(l.x1,l.x2)){
+            crossedHLine.push_back(l);
+        }
+    }
+
+    if(crossedHLine.size() == 0) return false;
+    std::sort(crossedHLine.begin(),crossedHLine.end(),[](line l1, line l2){return l1.y1 < l2.y1;});
+
+    return false;
+}
 
 const long long findArea(tileLocation p1, tileLocation p2){
     long dx = std::abs(p1.x-p2.x) + 1;
     long dy = std::abs(p1.y-p2.y) + 1;
     return dx*dy;
-}
-
-bool checkGreen(tileLocation p, std::vector<tileLocation>& redTiles){
-    // todo: find all lines from red -> red that intersect with a horizontal line across grid
-    //       determine if p is on line: if yes then true
-    //       determine if p is between lines 
-    std::vector<line> lines;
-    for(auto tile = redTiles.begin(); tile < redTiles.end()-1; tile++){
-        // check if line created between two tiles crosses this x 
-        if(p.x >= std::min(tile->x,(tile+1)->x) && p.x <= std::max(tile->x,(tile+1)->x) && tile->y == (tile+1)->y){
-            line newLine;
-            newLine.x1 = std::min(tile->x,(tile+1)->x);
-            newLine.x2 = std::max(tile->x,(tile+1)->x);
-            newLine.y1 = std::min(tile->y,(tile+1)->y);
-            newLine.y2 = std::max(tile->y,(tile+1)->y);
-            if(p.y == tile->y) return true; // if on line, return true
-            lines.push_back(newLine);
-        }
-    }
-    std::sort(lines.begin(),lines.end(),[](line l1, line l2){return l1.y1 < l2.y1;});
-    for(auto line : lines){
-        printf("(%li,%li,%li,%li)",line.x1,line.x2,line.y1,line.y2);
-    }
-    printf("\n");
-    bool output = false;
-    for(auto line = lines.begin(); line->y1 < p.y && line < lines.end(); line++){
-        output = output ^ true;
-    }
-    return output;
-}
-
-bool checkAreaGreen(tileLocation p1, tileLocation p2, std::vector<tileLocation>& redTiles){
-    bool green = true;
-    for(int i = std::min(p1.x,p2.x); i < std::max(p1.x,p2.x); i++){
-        for(int j = std::min(p1.y,p2.y);j < std::max(p1.y,p2.y); j++){
-            green = green && checkGreen(tileLocation(i,j),redTiles);
-        }
-    }
-    return green;
 }
 
 int main(int argv, char** argc){
@@ -94,43 +93,70 @@ int main(int argv, char** argc){
         }
     }
 
-    long maxY = 0;
-    long maxX = 0;
-    for(auto tile : redTiles){
-        if(tile.x > maxX) maxX = tile.x;
-        if(tile.y > maxY) maxY = tile.y;
+
+    //for(auto t : redTiles){printf("%li,%li\n",t.x,t.y);}
+
+    std::set<long> xValues;
+    std::set<long> yValues;
+    for(auto t : redTiles){
+        xValues.insert(t.x);
+        yValues.insert(t.y);
     }
-    // create map of green and red tiles
-    std::vector<std::string> tileMap;
-    tileMap.reserve(maxY + 1);
-    for(int i = 0; i < maxY + 1; i++){
-        tileMap.push_back("");
-        tileMap[i].reserve(maxX + 1);
-        for(int j = 0; j < maxX + 1; j++){
-            tileMap[i].append(".");
+
+    std::map<long,long> condenseX;
+    std::map<long,long> uncondenseX;
+    long index = 0;
+    for(auto x : xValues){
+        condenseX.insert(std::pair<long,long>(x,index));
+        uncondenseX.insert(std::pair<long,long>(index,x));
+        ++index;
+    }
+
+    std::map<long,long> condenseY;
+    std::map<long,long> uncondenseY;
+    index = 0;
+    for(auto y : yValues){
+        condenseY.insert(std::pair<long,long>(y,index));
+        uncondenseY.insert(std::pair<long,long>(index,y));
+        ++index;
+    }
+
+    std::vector<tileLocation> condensedTiles;
+    for(auto t : redTiles){
+        condensedTiles.push_back(tileLocation(condenseX.find(t.x)->second,condenseY.find(t.y)->second));
+    }
+
+    for(auto t : condensedTiles){printf("%li,%li\n",t.x,t.y);}                                                    // print condensed points
+    //for(auto t : condensedTiles){printf("%li,%li\n",uncondenseX.find(t.x)->second,uncondenseY.find(t.y)->second);}// print uncondensed points
+
+    std::vector<line> hLines;
+    std::vector<line> vLines;
+
+    for(auto tile = condensedTiles.begin(); tile < condensedTiles.end()-1; ++tile){
+        auto nTile = tile + 1;
+        // same x, line vertical
+        if(tile->x == nTile->x){
+            vLines.push_back(line(tile->x,nTile->x,tile->y,nTile->y));
+        }
+        // same y, line horizontal
+        if(tile->y == nTile->y){
+            hLines.push_back(line(tile->x,nTile->x,tile->y,nTile->y));
         }
     }
-    for(auto tile : redTiles) tileMap[tile.y][tile.x] = '#';
 
-    for(auto s : tileMap){
-        printf("%s\n",s.c_str());
-    }
+    determineGreen(tileLocation(1,1),vLines,hLines);
 
-    //for(auto s : tileMap) printf("%s\n",s.c_str());
-    //for(auto redTiles.begin)
-
-    // Now only red or green tiles
-    //printf("%i\n",checkGreen(tileLocation(9,6),redTiles));
-    // long long maxArea = 0;
-    // for(int i = 0; i < redTiles.size(); i++){
-    //     for(int j = 0; j < redTiles.size(); j++){
-    //         if(maxArea < findArea(redTiles[i],redTiles[j]) && checkAreaGreen(redTiles[i],redTiles[j],redTiles)){
-    //             maxArea = findArea(redTiles[i],redTiles[j]);
-    //         }
-    //     }
+    // for(auto l : hLines){
+    //     printf("(%li,%li,%li,%li)\n",l.x1,l.x2,l.y1,l.y2);
+    // }
+    // for(auto l : vLines){
+    //     printf("(%li,%li,%li,%li)\n",l.x1,l.x2,l.y1,l.y2);
     // }
 
+    long long maxAreaGreen = 0;
+
     printf("maxArea: %lli\n",maxArea);
+    printf("maxAreaGreen: %lli\n",maxAreaGreen);
 
     return 0;
 }
